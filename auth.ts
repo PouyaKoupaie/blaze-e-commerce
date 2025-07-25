24929ba7460cd433dcd97524be38470d3bfca731
Pouya <titanpooyanote@gmail.com>
2025-07-26 00:00:57 +0330

    user role added to session

--- auth.ts
+++ auth.ts
@@ -55,13 +55,32 @@ export const config = {
     async session({ session, token, user, trigger }: any) {
         // set the user ID from the token
         session.user.id = token.sub;
-
+        session.user.role = token.role;
+        session.user.name = token.name;
+        
         // if there is an update, set the name
         if (trigger === 'update') {
             session.user.name = user.name
         }
       return session;
     },
+    async jwt({token, user, trigger, session}: any) {
+      if (user) {
+        token.role = user.role
+        
+        //if user had no name then use the email
+        if(user.name === 'NO_NAME'){
+          token.name = user.email!.split('@')[0];
+
+          //update database to reflect the token name
+          await prisma.user.update({
+            where: {id: user.id},
+            data: {name: token.name}
+          })
+        }
+      }
+      return token
+    }
   },
 } satisfies NextAuthConfig;
 
